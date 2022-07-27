6abbbd8146652d0ffa9dbb950fa0e8c7ae8a751c
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-07-27 19:50:35 +0530

    Added API key; Use ENV var;

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import styles from './components/top.css';
 import axios from "axios";
 import TopDiv from "./components/Top";
 import MidDiv from "./components/Middle";
 import BottomDiv from "./components/Bottom";
 import { useState } from 'react';
-let list=[
-  {
-    title:"Hello",
-    desc: "Description",
-    link:"https://"
-  },
-  {
-    title:"Hello2",
-    desc: "Description2",
-    link:"https://2"
-  },
-  {
-    title:"Hello3",
-    desc: "Description3",
-    link:"https://3"
-  },
-  {
-    title:"Hello2",
-    desc: "Description2",
-    link:"https://2"
-  }
-]
+
 const App=()=>{
   const [list,setList]=useState([]);
+
+  const URL = process.env.API_URL || "https://newsapi.org";
   const fetchNews=(name)=>{
-    axios.get("https://newsapi.org/v2/everything?q=" + name+  "&apiKey=********************************")
+    axios.get(`${URL}/v2/everything?q=` + name+  "&apiKey=e95cb407d5d84bbb865d014c4b251e7a")
     .then(r=>{
       setList(r.data.articles);
     })
