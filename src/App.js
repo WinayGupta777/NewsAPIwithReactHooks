c6a4f0f78f6d24e44553a8950f010722aa2f77f1
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-07-27 23:45:47 +0530

    Variable changed

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { useState } from 'react';
 const App=()=>{
   const [list,setList]=useState([]);
 
-  const URL = process.env.API_URL || "https://newsapi.org";
+  const DOMAIN = process.env.REACT_APP_DOMAIN || "https://newsapi.org";
+  console.log(DOMAIN);
   const fetchNews=(name)=>{
-    axios.get(`${URL}/v2/everything?q=` + name+  "&apiKey=e95cb407d5d84bbb865d014c4b251e7a")
+    axios.get(`${DOMAIN}/v2/everything?q=` + name+  "&apiKey=e95cb407d5d84bbb865d014c4b251e7a")
     .then(r=>{
       setList(r.data.articles);
     })
   }
+
   return(
     <div>
       <TopDiv></TopDiv>
